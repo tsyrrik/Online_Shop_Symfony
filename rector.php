a624b913e402c1c9eab7ade3653c3f9389f3134f
Tsyren <Dashidymbrylov>
2025-02-17 23:17:25 +0700

    add new config rector

--- rector.php
+++ rector.php
@@ -17,3 +17,26 @@ return RectorConfig::configure()
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
+
+
+//declare(strict_types=1);
+//
+//use Rector\Config\RectorConfig;
+//use Rector\Php80\Rector\Class_\StringableForToStringRector;
+//use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+//
+//return RectorConfig::configure()
+//    ->withPaths([
+//        __DIR__ . '/bin/console',
+//        __DIR__ . '/config',
+//        __DIR__ . '/public',
+//        __DIR__ . '/src',
+//        __DIR__ . '/tests',
+//    ])
+//    ->withParallel()
+//    ->withCache(__DIR__ . '/var/rector')
+//    ->withPhpSets(php83: true)
+//    ->withSkip([
+//        StringableForToStringRector::class,
+//        AddOverrideAttributeToOverriddenMethodsRector::class,
+//    ]);
