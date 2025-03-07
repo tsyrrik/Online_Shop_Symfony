15aefe059ab8b23a31f82bae597cc7536740333b
Tsyren <Dashidymbrylov>
2025-03-07 16:19:51 +0700

    fix gitlab-ci.yml

--- app/public/index.php
+++ app/public/index.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(path: __DIR__) . '/vendor/autoload_runtime.php';
 
-return static fn(array $context) => new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return static fn(array $context) => new Kernel(environment: $context['APP_ENV'], debug: (bool) $context['APP_DEBUG']);
