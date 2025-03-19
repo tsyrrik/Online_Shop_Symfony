913cf1bd1f34264fe5171cca99d384c08a318a51
Tsyren <Dashidymbrylov>
2025-03-19 18:51:04 +0700

    add sendRegistrationNotification

--- app/src/Infrastructure/Api/Controller/RegistrationController.php
+++ app/src/Infrastructure/Api/Controller/RegistrationController.php
@@ -6,6 +6,9 @@ namespace App\Infrastructure\Api\Controller;
 
 use App\Application\Command\RegisterUserCommand;
 use App\Infrastructure\Api\Request\RegisterUserRequest;
+use Ramsey\Uuid\Uuid;
+use RdKafka\Producer;
+use RuntimeException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +17,15 @@ use Symfony\Component\Messenger\MessageBusInterface;
 
 final class RegistrationController extends AbstractController
 {
-    public function __construct(private MessageBusInterface $commandBus) {}
+    private MessageBusInterface $commandBus;
+
+    private Producer $kafkaProducer;
+
+    public function __construct(MessageBusInterface $commandBus, Producer $kafkaProducer)
+    {
+        $this->commandBus = $commandBus;
+        $this->kafkaProducer = $kafkaProducer;
+    }
 
     public function register(
         #[MapRequestPayload]
@@ -27,9 +38,31 @@ final class RegistrationController extends AbstractController
         );
         $this->commandBus->dispatch($registerCommand);
 
+        // Отправка уведомления в Kafka
+        $this->sendRegistrationNotification(userPhone: $registerRequest->phone);
+
         return new JsonResponse(
             data: ['status' => 'User registration initiated'],
             status: Response::HTTP_ACCEPTED,
         );
+    }
+
+    private function sendRegistrationNotification(string $userPhone): void
+    {
+        $topic = $this->kafkaProducer->newTopic(topic_name: 'user_registration');
+
+        $message = [
+            'type' => 'sms',
+            'userPhone' => $userPhone,
+            'promoId' => Uuid::uuid4()->toString(),
+        ];
+        $jsonPayload = json_encode(value: $message);
+
+        if ($jsonPayload === false) {
+            throw new RuntimeException(message: 'Failed to encode message to JSON');
+        }
+
+        $topic->produce(partition: RD_KAFKA_PARTITION_UA, msgflags: 0, payload: $jsonPayload);
+        $this->kafkaProducer->flush(timeout_ms: 10000);
     }
 }
